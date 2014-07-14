8466db6e9fd889a56680ffd438051a2e8d120993
Lance Fetters <gaizin@gmail.com>
2014-07-14 17:27:34 +0900

    Fixed NSString category, added to Makefile

--- Application/NSString+CrashReporter.m
+++ Application/NSString+CrashReporter.m
@@ -5,7 +5,7 @@
 - (NSString *)stripQuotes {
     NSUInteger length = [self length];
     if (length >= 2) {
-        if (([string characterAtIndex:0] == '"') && ([string characterAtIndex:(length - 1)] == '"')) {
+        if (([self characterAtIndex:0] == '"') && ([self characterAtIndex:(length - 1)] == '"')) {
             return [self substringWithRange:NSMakeRange(1, (length - 2))];
         }
     }
