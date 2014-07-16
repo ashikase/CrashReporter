7470641c342228df2c912590c1724adf1bf15ed0
Lance Fetters <gaizin@gmail.com>
2014-07-16 15:42:56 +0900

    Updated custom blame controller

--- Application/CustomBlameController.m
+++ Application/CustomBlameController.m
@@ -1,96 +1,111 @@
 /*
 
-CustomBlameConstroller.m ... Text editor for creating custom blame scripts.
-Copyright (C) 2009  KennyTM~ <kennytm@gmail.com>
+   CustomBlameConstroller.m ... Text editor for creating custom blame scripts.
+   Copyright (C) 2009  KennyTM~ <kennytm@gmail.com>
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
 #import "CustomBlameController.h"
-#import <UIKit/UIKit.h>
-#import "ReporterLine.h"
+
 #import "BlameController.h"
+#import "IncludeReporterLine.h"
+#import "LinkReporterLine.h"
 
-@implementation CustomBlameController
--(void)loadView {
-	textView = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, 320, 200)];
-	textView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+@interface CustomBlameController () <UIAlertViewDelegate>
+@end
 
-	textView.font = [UIFont fontWithName:@"Courier" size:[UIFont systemFontSize]];
-	textView.autocorrectionType = UITextAutocorrectionTypeNo;
-	textView.autocapitalizationType = UITextAutocapitalizationTypeNone;
-
-
-	UIView* superViewCollection = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 460)];
-	superViewCollection.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-	superViewCollection.autoresizesSubviews = YES;
-	superViewCollection.backgroundColor = [UIColor whiteColor];
-	[superViewCollection addSubview:textView];
-	[textView release];
-
-	self.view = superViewCollection;
-	[superViewCollection release];
-
-	UIBarButtonItem* done = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(submit)];
-	self.navigationItem.rightBarButtonItem = done;
-	[done release];
-
-	[textView becomeFirstResponder];
-
-	NSBundle* mainBundle = [NSBundle mainBundle];
-	self.title = [mainBundle localizedStringForKey:@"Script" value:nil table:nil];
-	UIAlertView* confirmDialog = [[UIAlertView alloc] initWithTitle:nil message:[mainBundle localizedStringForKey:@"CUSTOM_BLAME_WARNING"
-																											value:@"Warning: Entering invalid data may corrupt your system. Use this page only in guidance of the developer."
-																											table:nil]
-														   delegate:self
-												  cancelButtonTitle:[mainBundle localizedStringForKey:@"Back" value:nil table:nil]
-												  otherButtonTitles:[mainBundle localizedStringForKey:@"Continue" value:nil table:nil], nil];
-	[confirmDialog performSelector:@selector(show) withObject:nil afterDelay:0.1];
-	// confirmDialog's +1 retain count is intentional.
+@implementation CustomBlameController {
+    UITextView *textView_;
 }
 
--(void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-	CGRect f = textView.frame;
-	f.size.height = UIInterfaceOrientationIsLandscape(self.interfaceOrientation) ? 106 : 200;	// XXX: hard-coded values!
-	f.origin.y = 0;
-	textView.frame = f;
+- (void)loadView {
+    CGRect screenBounds = [[UIScreen mainScreen] bounds];
+
+    UITextView *textView = [[UITextView alloc] initWithFrame:CGRectMake(0.0, 0.0, screenBounds.size.width, screenBounds.size.height)];
+    textView.autocapitalizationType = UITextAutocapitalizationTypeNone;
+    textView.autocorrectionType = UITextAutocorrectionTypeNo;
+    textView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    textView.font = [UIFont fontWithName:@"Courier" size:[UIFont systemFontSize]];
+    [textView becomeFirstResponder];
+    textView_ = textView;
+
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0.0, 0.0, screenBounds.size.width, screenBounds.size.height)];
+    //view.backgroundColor = [UIColor whiteColor];
+    view.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [view addSubview:textView];
+    self.view = view;
+    [view release];
+
+    UIBarButtonItem* done = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(submit)];
+    self.navigationItem.rightBarButtonItem = done;
+    [done release];
+
+    NSBundle *mainBundle = [NSBundle mainBundle];
+    self.title = [mainBundle localizedStringForKey:@"Script" value:nil table:nil];
+
+    NSString *message = [mainBundle localizedStringForKey:@"CUSTOM_BLAME_WARNING"
+        value:@"Warning: Entering invalid data may corrupt your system. Use this page only in guidance of the developer."
+        table:nil];
+    UIAlertView *confirmDialog = [[UIAlertView alloc] initWithTitle:nil message:message delegate:self
+        cancelButtonTitle:[mainBundle localizedStringForKey:@"Back" value:nil table:nil]
+        otherButtonTitles:[mainBundle localizedStringForKey:@"Continue" value:nil table:nil], nil];
+    [confirmDialog performSelector:@selector(show) withObject:nil afterDelay:0.1];
+    // confirmDialog's +1 retain count is intentional.
 }
 
--(BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation { return interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown; }
-
--(void)alertView:(UIAlertView*)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
-	if (buttonIndex == alertView.cancelButtonIndex)
-		[self.navigationController popViewControllerAnimated:YES];
-	[alertView release];
+- (void)dealloc {
+    [textView_ release];
+    [super dealloc];
 }
 
--(void)submit {
-	NSArray* lines = [textView.text componentsSeparatedByString:@"\n"];
-	NSMutableArray* reporters = [[NSMutableArray alloc] init];
-	for (NSString* line in lines) {
-		ReporterLine* reporter = [ReporterLine reporterWithLine:line];
-		if (reporter)
-			[reporters addObject:reporter];
-	}
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    return interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown;
+}
 
-	[reporters sortUsingSelector:@selector(compare:)];
+#pragma mark - UIAlertViewDelegate
 
-	BlameController* ctrler = [[BlameController alloc] initWithReporters:reporters packageName:nil authorName:nil suspect:nil isAppStore:NO];
-	[reporters release];
-	[self.navigationController pushViewController:ctrler animated:YES];
-	[ctrler release];
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if (buttonIndex == alertView.cancelButtonIndex) {
+        [self.navigationController popViewControllerAnimated:YES];
+    }
+    [alertView release];
+}
+
+- (void)submit {
+    LinkReporterLine *linkReporter = nil;
+    NSMutableArray *includeReporters = [NSMutableArray new];
+
+    NSArray *lines = [textView_.text componentsSeparatedByString:@"\n"];
+    Class $LinkReporterLine = [LinkReporterLine class];
+    for (NSString *line in lines) {
+        ReporterLine *reporter = [ReporterLine reporterWithLine:line];
+        if (reporter != nil) {
+            if ([reporter isKindOfClass:$LinkReporterLine]) {
+                linkReporter = [LinkReporterLine reporterWithLine:line];
+            } else {
+                [includeReporters addObject:reporter];
+            }
+        }
+    }
+
+    BlameController *controller = [[BlameController alloc] initWithPackage:nil suspect:nil linkReporter:linkReporter includeReporters:includeReporters];
+    [self.navigationController pushViewController:controller animated:YES];
+    [controller release];
+
+    [includeReporters release];
 }
 
 @end
